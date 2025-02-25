8791c2c1b0218c0989479bf38ec4c818e7ec638f
Alicia <96835188+Animanga178@users.noreply.github.com>
2025-02-25 17:55:14 +0000

    Water respawn fixed, log sprites added with consistent movement

--- Assets/Water.cs
+++ Assets/Water.cs
@@ -3,12 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Water : MonoBehaviour
 {
+    public Transform respawnPoint;
     void OnTriggerEnter2D (Collider2D collision)
     {
         if (collision.tag == "Frog") 
         {
+            collision.transform.position = respawnPoint.position;
             Debug.Log("Sink");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
