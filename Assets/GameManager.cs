f5cca7054cd83b02ca191dda1f59f1622b23ba4b
Alicia <96835188+Animanga178@users.noreply.github.com>
2025-04-09 11:56:07 +0100

    Added GameManager script to handle game state, level progression, and scoring

---
+++ Assets/GameManager.cs
@@ -0,0 +1,169 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private Home[] homes;
+
+    private PlayerController player;
+
+    public GameObject gameOverMenu;
+
+    [SerializeField] private TextMeshProUGUI livesText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    public int lives { get; private set; } = 3;
+    public int score { get; private set; } = 0;
+    public int time { get; private set; } = 30;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    homes = Object.FindObjectsByType<Home>(FindObjectsSortMode.None);
+    player = FindFirstObjectByType<PlayerController>();
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        gameOverMenu.SetActive(false);
+        SetScore(0);
+        SetLives(3);
+        NewLevel();
+    }
+
+    private void NewLevel()
+    {
+        for (int i = 0; i < homes.Length; i++)
+        {
+            homes[i].enabled = false;
+        }
+
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        player.Respawn();
+        StopAllCoroutines();
+        StartCoroutine(Timer(30));
+    }
+
+    private IEnumerator Timer(int duration)
+    {
+        time = duration;
+        timeText.text = time.ToString();
+
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1);
+
+            time--;
+            timeText.text = time.ToString();
+        }
+
+        player.Death();
+    }
+
+    public void MovedUp()
+    {
+        SetScore(score + 10);
+    }
+
+    public void PlayerDeath()
+    {
+        SetLives(lives - 1);
+
+        if (lives > 0)
+        {
+            Invoke(nameof(Respawn), 1f);
+        }
+        else
+        {
+            Invoke(nameof(GameOver), 1f);
+        }
+    }
+
+    private void GameOver()
+    {
+        player.gameObject.SetActive(false);
+        gameOverMenu.SetActive(true);
+
+        StopAllCoroutines();
+        StartCoroutine(PlayAgain());
+    }
+
+    private IEnumerator PlayAgain()
+    {
+        bool playAgain = false;
+
+        while (!playAgain) 
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                playAgain = true;
+            }
+            yield return null;
+        }
+        NewGame();
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+        livesText.text = lives.ToString();
+    }
+
+    public void HomeOccupied()
+    {
+        player.gameObject.SetActive(false);
+
+        int bonus = time * 20;
+        SetScore(score + bonus + 50);
+       
+        if (Cleared())
+        {
+            SetScore(score + 1000);
+            Invoke(nameof(NewLevel), 1f);
+        }
+        else
+        {
+            Invoke(nameof(Respawn), 1f);
+        }
+    }
+
+    private bool Cleared()
+    {
+        for (int i = 0; i < homes.Length; i++)
+        {
+            if (!homes[i].enabled)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
