d75cede03af082d61747b18034deb65e70c9cf35
Alicia <96835188+Animanga178@users.noreply.github.com>
2025-04-09 11:54:28 +0100

    Added Home script to manage player occupation of safe zones

---
+++ Assets/Home.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    public GameObject homePlayer;
+
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnEnable()
+    {
+        homePlayer.SetActive(true);
+        //boxCollider.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        homePlayer.SetActive(false);
+        //boxCollider.enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!enabled && collision.gameObject.CompareTag("Player"))
+        {
+            enabled = true;
+            GameManager.Instance.HomeOccupied();
+
+        }
+    }
+}
