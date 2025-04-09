e1322ebc88190a15aa9224fa109ed0e29c878f38
Alicia <96835188+Animanga178@users.noreply.github.com>
2025-04-09 11:56:59 +0100

    Merged CameraEdges script into CameraController and refactored camera movement and boundary logic

---
+++ Assets/CameraController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform leftBarrier;
+    [SerializeField] private Transform rightBarrier;
+
+    private Vector3 initalCameraPosition;
+    private Vector3 velocity = Vector3.zero;
+
+    private float smoothTime = 0.25f;
+    private float minY;
+    public float LeftEdge { get; private set; }
+    public float RightEdge { get; private set; }
+    public float BottomEdge { get; private set; }
+
+    private void Start()
+    {
+        initalCameraPosition = transform.position;
+    }
+    private void Update()
+    {
+        Vector3 targetPosition = target.position + initalCameraPosition;
+
+        float camHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, leftBarrier.position.x + camHalfWidth, rightBarrier.position.x - camHalfWidth);
+
+        BottomEdge = Camera.main.transform.position.y - Camera.main.orthographicSize;
+        LeftEdge = Camera.main.transform.position.x - camHalfWidth;
+        RightEdge = Camera.main.transform.position.x + camHalfWidth;
+
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minY, float.MaxValue);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+
+    //public float GetBottomEdge()
+    //{
+    //    return bottomEdge;
+    //}
+}
